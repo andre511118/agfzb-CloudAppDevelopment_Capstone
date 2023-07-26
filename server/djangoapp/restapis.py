aa4683f9da97fb2566d3041f21232ca2cb065211
“andre511118” <yaozhang@live.de>
2023-07-26 03:53:16 -0400

    add analyze_review_sentiments

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,11 +10,15 @@ from requests.auth import HTTPBasicAuth
 #                                     auth=HTTPBasicAuth('apikey', api_key))
 def get_request(url, **kwargs):
     print(kwargs)
-    print("GET from {} ".format(url))
+    print("GET from {} ".format(url))    
     try:
-        # Call get method of requests library with URL and parameters
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                    params=kwargs)
+        if api_key:
+            # Basic authentication GET
+            request.get(url, params=kwargs, headers={'Content-Type': 'application/json'},
+                                        auth=HTTPBasicAuth('apikey', api_key))
+        else:
+            # no authentication GET
+            request.get(url, params=kwargs)
     except:
         # If any error occurs
         print("Network exception occurred")
@@ -34,8 +38,11 @@ def get_request(url, **kwargs):
 # - Parse JSON results into a CarDealer object list
 def get_dealers_from_cf(url, **kwargs):
     results = []
-    # Call get_request with a URL parameter
-    json_result = get_request(url)
+    state = kwargs.get("state")
+    if state:
+        json_result = get_request(url, state=state)
+    else:
+        json_result = get_request(url)
     if json_result:
         # Get the row list in JSON as dealers
         dealers = json_result["rows"]
@@ -62,13 +69,21 @@ def get_dealer_reviews_from_cf(url, dealerId):
         reviews = json_result["entries"]
         for review in reviews:
             review_obj = DealerReview(dealerId=review["dealerId"], review=review["review"], sentiment="")  # Create a DealerReview object
+            review_obj.sentiment = analyze_review_sentiments(review_obj.review)
             results.append(review_obj)
     return results
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-# def analyze_review_sentiments(text):
+#def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
-
-
+def analyze_review_sentiments(text): 
+    url = "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/bfff7283-7baa-4638-88d6-761d50619008" 
+    api_key = "sMbvHE7ObcPwKMhw-1EKJjG4pOzeDYQSvgyn0bZZl-q9" 
+    authenticator = IAMAuthenticator(api_key) 
+    natural_language_understanding = NaturalLanguageUnderstandingV1(version='2021-08-01',authenticator=authenticator) 
+    natural_language_understanding.set_service_url(url) 
+    response = natural_language_understanding.analyze( text=text+"hello hello hello",features=Features(sentiment=SentimentOptions(targets=[text+"hello hello hello"]))).get_result() 
+    label=json.dumps(response, indent=2) 
+    label = response['sentiment']['document']['label'] 
+    return(label)
